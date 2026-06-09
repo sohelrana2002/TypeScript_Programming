3e7be8cd074734f659151cc51aaf33c5252fe828
SOHEL RANA <sohelrana104322@gmail.com>
2026-06-09 22:59:20 +0600

    TS Array

---
+++ src/Arrays/Arrays.ts
@@ -0,0 +1,12 @@
+const name: string[] = [];
+name.push("Sohel");
+name.push("Rana");
+
+console.log(name);
+
+// Readonly
+// - The readonly keyword can prevent arrays from being changed.
+const userName: readonly string[] = ["Rolex"];
+// userName.push("Suria");
+// The readonly keyword can prevent arrays from being changed.
+console.log(userName);
